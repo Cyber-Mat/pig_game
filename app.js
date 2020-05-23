faad9d7cce5bed888ef89d74af7399f6e01f23a4
cyber-mat <olaniyanolamide@gmail.com>
2020-05-23 19:02:48 +0100

    Basic dice roll

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+let scores, totalScore, activePlayer;
+
+scores = [0,0];
+totalScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+function btn () {
+    // 1. Generate random number for each dice roll
+    let dice = Math.floor(Math.random() * 6) + 1; 
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+    
+    //2. Display generated dice number
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.setAttribute('src', 'dice-' + dice + '.png');
+    
+    //3. Update total score if dice roll > 1
+    if (dice>1) {
+        document.querySelector('#score-' + activePlayer).textContent = totalScore + dice;
+    } else {
+        document.querySelector('#score-' + activePlayer).textContent = totalScore;
+    }
+    
+} 
+
+document.querySelector('.btn-roll').addEventListener('click', btn);
